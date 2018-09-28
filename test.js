005bba60593c9dae0fd8c8b7ccbae360f39d8aad
unknown <C:\Users\\AppData\Roaming\The Bat!>
2018-09-28 20:47:30 +0700

    test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({ show: true });
+
+nightmare
+.goto('https://reactjs.org/tutorial/tutorial.html')
+.wait(3000)
+.evaluate(function () {
+	var page = document.querySelector('html').innerHTML;
+	return page
+})
+.end()
+.then(function (page) {
+	//console.log(result)
+	fs.writeFile('files/page.html', page, function (err) {
+		if (err)
+			return console.log(err);
+		console.log('Done');
+	});
+})
